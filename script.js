5773831cd306cb6c563609237c856b666dc1537b
German Renato Cobian <germancobian@hotmail.com>
2023-07-06 15:14:52 -0700

    Resolve linter issues

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-use-before-define, no-unused-vars, prefer-destructuring, eqeqeq */
 
-const AppCode = 'ni4kbDviF90gEYVZhT5F'
+const AppCode = 'ni4kbDviF90gEYVZhT5F';
 const commentsURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${AppCode}/comments`;
 const likesURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${AppCode}/likes`;
 
